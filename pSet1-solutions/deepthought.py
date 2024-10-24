c30069fe789def13331ea3dfb094eecede58c017
Muhammed <137482384+muhammedmozbey@users.noreply.github.com>
2024-10-24 23:35:46 +0300

    pSet1

---
+++ pSet1-solutions/deepthought.py
@@ -0,0 +1,5 @@
+ranInput = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if ranInput == "42" or ranInput == "forty-two" or ranInput == "forty two":
+    print("Yes!")
+else:
+    print("No!")
